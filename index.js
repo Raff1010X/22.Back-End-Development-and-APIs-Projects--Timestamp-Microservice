8c7af5b06b1e4ee633a48f763e83a2cfa0b64eff
Rafał Kowalczyk <raff1410X@gmail.com>
2022-05-09 19:14:40 +0200

    HEROKU final commit

--- index.js
+++ index.js
@@ -13,30 +13,9 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-    res.json({ greeting: 'hello API' });
-});
+const timestampRouter = require('./routes/timestampRoutes');
 
-// timestime enpoint
-app.get('/api/:input', function (req, res) {
-    let input = req.params.input;
-
-    // check if input is unix then convert to date
-    if (Number.isInteger(Number(input))) input = new Date(Number(input));
-
-    // resolve unix from date
-    let unix = Date.parse(input);
-
-    // convert unix timestamp to utc date
-    let utc = new Date(unix).toUTCString();
-
-    if (isNaN(unix)) {
-        res.json({ error: 'Invalid date' });
-    } else {
-        res.json({ unix, utc });
-    }
-});
+app.use('/api', timestampRouter);
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
