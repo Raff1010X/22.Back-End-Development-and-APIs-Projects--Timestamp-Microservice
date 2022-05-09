8116c406b81c91ea07487f6cf653072c242435fd
Rafał Kowalczyk <raff1410X@gmail.com>
2022-05-09 18:06:34 +0200

    final commit

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// index.js
-// where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-
-var cors = require('cors');
+// enable CORS
+// so that API is remotely testable by FCC
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
@@ -24,22 +18,27 @@ app.get('/api/hello', function (req, res) {
     res.json({ greeting: 'hello API' });
 });
 
-// your first API endpoint...
-app.get('/api/:date', function (req, res) {
-    let date_input = req.params.date;
-    // check if date is a valid date
-    if (isNaN(Date.parse(date_input))) {
+// timestime enpoint
+app.get('/api/:input', function (req, res) {
+    let input = req.params.input;
+
+    // check if input is unix then convert to date
+    if (Number.isInteger(Number(input))) input = new Date(Number(input));
+
+    // resolve unix from date
+    let unix = Date.parse(input);
+
+    // convert unix timestamp to utc date
+    let utc = new Date(unix).toUTCString();
+
+    if (isNaN(unix)) {
         res.json({ error: 'Invalid date' });
     } else {
-        // convert date to unix timestamp
-        let unix = Date.parse(date_input) / 1000;
-        // convert unix timestamp to date
-        let utc = new Date(unix * 1000).toUTCString();
         res.json({ unix, utc });
     }
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
 });
